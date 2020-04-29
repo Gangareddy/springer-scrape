165f511adb70dc79850def37b32ca1365ef3423a
Ganga Reddy Tankasala <gtankasa@.starbucks.net>
2020-04-29 14:02:19 -0700

    Adding links and code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# springer-scrape
+# Download all free Springer books from links 
+
+## Sample file `springer_links.txt` contains links need to be downloaded 
+
+```
+http://link.springer.com/openurl?genre=book&isbn=978-3-662-44874-8
+http://link.springer.com/openurl?genre=book&isbn=978-3-319-13072-9
+http://link.springer.com/openurl?genre=book&isbn=978-3-319-47831-9
+http://link.springer.com/openurl?genre=book&isbn=978-1-4612-1844-9
+```
+
+Links of books can be found in `Free+English+textbooks.xlsx`
+
+## Install requirement packages
+```
+pip install -r requirements.txt
+```
+
+## Usage: `python download.py <sample_file> <output_folder>`
+```
+python download.py springer_links.txt books
+```
---
+++ download.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import wget
+import os
+import codecs
+import sys
+
+def extract_pdf_link(
+        url="http://link.springer.com/openurl?genre=book&isbn=978-3-662-44874-8",
+        saved_folder="download"):
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    item = soup.find('a', {'title': 'Download this book in PDF format'})
+    pdf_link = "http://link.springer.com/" + item['href']
+    nameElement = soup.find('div', attrs={'data-test': 'book-title', 'class': 'page-title'})
+    text = nameElement.find('h1').text.replace(" ", "_")
+    # file_name = url[url.rfind("=")+1:]
+    wget.download(pdf_link, "{}/{}.pdf".format(saved_folder, text))
+    return pdf_link, text
+
+if __name__ == '__main__':
+    springer_link_file, saved_folder = (sys.argv[1], sys.argv[2]) if len(sys.argv) > 2 else ("springer_links.txt", "download")
+
+    if not os.path.exists(saved_folder):
+        os.mkdir(saved_folder)
+
+    with codecs.open(springer_link_file, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+        for i, link in enumerate(lines):
+            try:
+                link = link.strip()
+                print("downloading ... {}".format(link))
+                pdf_link, name = extract_pdf_link(link, saved_folder)
+                #name = extract_pdf_name(link)
+                print(name)
+                print("\ndownloaded {}/{}: {} (pdf: {})".format(i, len(lines), link, pdf_link))
+            except:
+                print("error: ", link)
